37cef9c6759b08dcdca77aac2544936f9f0f6922
ShriramShagri <shagrisrm@gmail.com>
2020-10-05 22:36:15 +0530

    feat: Basic template added

---
+++ Basics/basicTemplate.py
@@ -0,0 +1,19 @@
+import pygame
+
+WIDTH, HEIGHT = 400, 400
+
+pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Basic Template')
+
+
+def main():
+    run = False
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    
+
+if __name__ == "__main__":
+    main()
